410164137ad3a7a8283f57afda3e0728bb51c6c6
Leonan Yglecias Mattos <leonanmattos@gmail.com>
2021-12-19 14:44:13 -0300

    database configured

--- src/main/kotlin/com/mercadolivro/controller/request/PostCustomerRequest.kt
+++ src/main/kotlin/com/mercadolivro/controller/request/PostCustomerRequest.kt
@@ -1,5 +1,7 @@
 package com.mercadolivro.controller.request
 
+import com.mercadolivro.model.CustomerModel
+
 class PostCustomerRequest(
     var name: String,
     var email: String
--- src/main/kotlin/com/mercadolivro/service/CustomerService.kt
+++ src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -1,4 +1,44 @@
 package com.mercadolivro.service
 
+import com.mercadolivro.model.CustomerModel
+import org.springframework.stereotype.Service
+
+@Service
 class CustomerService {
+
+    val customers = mutableListOf<CustomerModel>()
+
+    fun getAll(name: String?): List<CustomerModel> {
+        name?.let {
+            return customers.filter { it.name.contains(name, true) }
+        }
+        return customers
+    }
+
+    fun create(customer: CustomerModel) {
+        val id = if (customers.isEmpty()) {
+            1
+        } else {
+            customers.last().id!!.toInt() + 1
+        }.toString()
+
+        customer.id = id
+
+        customers.add(customer)
+    }
+
+    fun getCustomer(id: String): CustomerModel {
+        return customers.first { it.id == id }
+    }
+
+    fun update(customer: CustomerModel) {
+        customers.first { it.id == customer.id }.let {
+            it.name = customer.name
+            it.email = customer.email
+        }
+    }
+
+    fun delete(id: String) {
+        customers.removeIf { it.id == id }
+    }
 }
